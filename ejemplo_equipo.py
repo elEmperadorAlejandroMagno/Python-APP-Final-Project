effcf10862b03aa1cbad0ae04b4cd44962c6d818
Alejandro Marvich <alejandromarvich181100@gmail.com>
2025-08-03 02:03:39 +0000

    feat: añadir funcionalidad específica para equipo

---
+++ ejemplo_equipo.py
@@ -0,0 +1,8 @@
+# Ejemplo de trabajo en equipo
+def nueva_funcionalidad():
+    """Ejemplo de nueva funcionalidad desarrollada en rama."""
+    return "Esta es una nueva funcionalidad"
+
+def commit_especifico():
+    """Esta función será parte de un commit específico."""
+    return "Commit específico para el equipo"
